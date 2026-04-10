389178e6ab829db699e6fccf70c2edfbcc70ca71
Jonathan Jeremiah <jeremiahjonathan372@gmail.com>
2026-04-10 13:46:19 +0100

    fix: add predeploy script and fix homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignIn from "./components/SignIn";
 import FileUploader from "./components/FileUploader";
@@ -8,14 +8,14 @@ import Menu from "./components/menu";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/fileuploader" element={<FileUploader />} />
         <Route path="/filelist" element={<FileList />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
